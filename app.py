5d060bf5511992604f19ae7cb03cb98828d5c2e4
Shagun Kala <kala.shagun@gmail.com>
2023-05-03 18:00:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application=Flask(__name__)
+
 app=application
 
 ## Route for a home page
