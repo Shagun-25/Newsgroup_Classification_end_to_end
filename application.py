c8215a64dc83e9c9dfc3a2937e484ff651e3925e
Shagun-25 <kala.shagun@gmail.com>
2023-05-03 16:31:21 +0530

    Added port

--- application.py
+++ application.py
@@ -38,4 +38,4 @@ def predict_datapoint():
         return render_template('home.html',results=d[results])
     
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=8080)
